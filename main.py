3d06331f62b20c8961982140d85260acf9e686c6
EmiRoberti77 <emiliano.roberti@accenture.com>
2025-05-19 20:49:04 +0100

    extracting face embedding

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from deepface import DeepFace
 
 def verify():
+  """
+  function to compare two faces and work out if there is a match based on a threashold
+  """
   print("starting face comparison")
   result = DeepFace.verify(
     img1_path='./images/face1.png',
@@ -10,8 +13,18 @@ def verify():
   print(result)
 
 def find(img_path, db_path):
+  """
+  function to look for a face inside a file system folder structure
+  """
   dfs = DeepFace.find(img_path=img_path, db_path=db_path)
   print(dfs)
 
- 
-find("./images/face1.png", "my_db")
+def represent(img_path):
+  embedding_objs = DeepFace.represent(img_path=img_path)
+  for embedding_obj in embedding_objs:
+    embedding = embedding_obj["embedding"]
+    print(embedding)
+
+
+#find("./images/face1.png", "my_db")
+represent("./images/face1.png")
