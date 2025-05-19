cc691fd60730ce0c4646c57a5520a5383880b56e
EmiRoberti77 <emiliano.roberti@accenture.com>
2025-05-19 20:30:15 +0100

    testing searching into a face file structure database

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from deepface import DeepFace
-print("starting face comparison")
-result = DeepFace.verify(
-  img1_path='./images/face1.png',
-  img2_path='./images/face3.png'
-)
-print("completed face comparison")
-print(result)
+
+def verify():
+  print("starting face comparison")
+  result = DeepFace.verify(
+    img1_path='./images/face1.png',
+    img2_path='./images/face3.png'
+  )
+  print("completed face comparison")
+  print(result)
+
+def find(img_path, db_path):
+  dfs = DeepFace.find(img_path=img_path, db_path=db_path)
+  print(dfs)
+
+ 
+find("./images/face1.png", "my_db")
